767f0cd2de2b987fb2ec047ad69b88afd1d40b71
PeterKovacs <kope8899@gmail.com>
2018-02-02 17:10:50 +0100

    Initial Commit

---
+++ HugeInt/HugeInt.h
@@ -0,0 +1,19 @@
+//
+//  HugeInt.h
+//  HugeInt
+//
+//  Created by PeterKovacs on 2018. 02. 02..
+//  Copyright © 2018. KP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HugeInt.
+FOUNDATION_EXPORT double HugeIntVersionNumber;
+
+//! Project version string for HugeInt.
+FOUNDATION_EXPORT const unsigned char HugeIntVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HugeInt/PublicHeader.h>
+
+
